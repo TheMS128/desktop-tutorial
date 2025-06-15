21044018b9b2b65e21974104059ec2c814deecd5
TheMS128 <onisimchuk.bogdan@gmail.com>
2025-06-15 14:06:04 +0300

    New commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("io.qameta.allure") version "2.12.0"
 }
 
 group = "org.example"
@@ -7,13 +8,25 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    gradlePluginPortal()
 }
 
 dependencies {
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation(platform("org.junit:junit-bom:5.13.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+
+    testImplementation("org.hamcrest:hamcrest:3.0")
+    implementation("org.seleniumhq.selenium:selenium-java:4.33.0")
+    testImplementation("io.qameta.allure:allure-junit5:2.29.1")
 }
 
 tasks.test {
     useJUnitPlatform()
+
+    doFirst {
+        delete("build/reports")
+        delete("build/allure-results")
+    }
+
+    finalizedBy("allureReport")
 }
